5ce9b6316f967a4d2ee71edb89e64e0f5bdb7e68
Josu Ruiz <cyrusnimda@gmail.com>
2026-04-08 10:02:03 +0100

    add security middleware and security.txt file

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { defineMiddleware } from 'astro:middleware';
+
+export const onRequest = defineMiddleware(async (_context, next) => {
+    const res = await next();
+
+    res.headers.set(
+        'Content-Security-Policy',
+        [
+            "default-src 'self'",
+            "script-src 'self' 'unsafe-inline' https://challenges.cloudflare.com",
+            "style-src 'self' 'unsafe-inline'",
+            "img-src 'self' data:",
+            "font-src 'self'",
+            "connect-src 'self' https://challenges.cloudflare.com",
+            "frame-src https://challenges.cloudflare.com",
+            "frame-ancestors 'none'",
+        ].join('; ')
+    );
+    res.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+    res.headers.set('X-Content-Type-Options', 'nosniff');
+    res.headers.set('X-Frame-Options', 'DENY');
+    res.headers.set('X-XSS-Protection', '1; mode=block');
+
+    return res;
+});
