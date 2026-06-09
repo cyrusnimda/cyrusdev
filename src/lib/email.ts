30cb8ac2cf29d1e60e94b291cee61ee20e6ae036
Josu Ruiz <cyrusnimda@gmail.com>
2026-06-09 13:06:26 +0100

    Add confeti on feedback submit

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -8,7 +8,7 @@ const mg = mailgun.client({ username: 'api', key: MAILGUN_API_KEY });
 export async function sendContactEmail(name: string, email: string, message: string) {
     await mg.messages.create('mg.cyrusnimda.com', {
         to: 'contact@cyrusdev.co.uk',
-        from: 'postmaster@mg.cyrusnimda.com',
+        from: 'Cyrusdev <postmaster@mg.cyrusnimda.com>',
         subject: `New contact from ${name} - cyrusdev.co.uk`,
         text: `Name: ${name}\nEmail: ${email}\n\n${message}`,
         html: `<p><strong>Name:</strong> ${name}</p><p><strong>Email:</strong> ${email}</p><p>${message}</p>`,
